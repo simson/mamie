31d732c127c8482a329df7758d93dde2872577f5
Siméon Marijon <simeon@marijon.fr>
2012-07-20 00:31:52 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mamie::Application.config.secret_token = '9d96bcca5557f1dfe94128c492618acfdc51d8af2a495ef766d0af1255f8e6affeff2872a6b82749ca4373be7cd825763e042f6acb171815238b08c3e4802d1a'
